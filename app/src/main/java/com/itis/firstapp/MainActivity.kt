0432d8c1f5101dafc631c1b2b2e8903b95eb8297
Elina Kadyrova <kad_elina@mail.ru>
2021-10-04 23:13:54 +0300

    Activity done.

--- app/src/main/java/com/itis/firstapp/MainActivity.kt
+++ app/src/main/java/com/itis/firstapp/MainActivity.kt
@@ -2,8 +2,13 @@ package com.itis.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var buttonEdit: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
