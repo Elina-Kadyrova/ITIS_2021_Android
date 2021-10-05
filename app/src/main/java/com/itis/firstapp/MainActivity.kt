a5798449e0b8d606829d814984ea33e52c12907b
Elina Kadyrova <kad_elina@mail.ru>
2021-10-05 23:42:28 +0300

    EditText done.

--- app/src/main/java/com/itis/firstapp/MainActivity.kt
+++ app/src/main/java/com/itis/firstapp/MainActivity.kt
@@ -2,15 +2,43 @@ package com.itis.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import com.itis.firstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var buttonEdit: Button? = null
+    private lateinit var binding: ActivityMainBinding
+    private var isBtnPressed: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        with(binding){
+            btnEdit.setOnClickListener {
+
+                if(!isBtnPressed){
+                    tvUsersDescription.setVisibility(View.GONE)
+                    etUsersDescription.setVisibility(View.VISIBLE)
+                    isBtnPressed = true
+                } else {
+                    tvUsersDescription.text = etUsersDescription.text
+                    (getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager).apply {
+                        hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+                    }
+                    etUsersDescription.setVisibility(View.GONE)
+                    tvUsersDescription.setVisibility(View.VISIBLE)
+                    isBtnPressed = false
+                }
+            }
+        }
+
     }
 }
