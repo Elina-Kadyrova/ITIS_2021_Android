7efd68ab5acc4ade5cd336237abf336f68f4b12b
Elina Kadyrova <kad_elina@mail.ru>
2021-10-15 21:23:33 +0300

    Second part of homework done.

--- app/src/main/java/com/itis/firstapp/SecondActivity.kt
+++ app/src/main/java/com/itis/firstapp/SecondActivity.kt
@@ -1,12 +1,7 @@
 package com.itis.firstapp
 
-import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
-import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.itis.firstapp.databinding.ActivitySecondBinding
@@ -14,7 +9,6 @@ import com.itis.firstapp.databinding.ActivitySecondBinding
 class SecondActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivitySecondBinding
-    private lateinit var butMap: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,20 +17,13 @@ class SecondActivity: AppCompatActivity() {
         }
 
         intent?.also {
-            print("hiii")
-        }
-    }
-
-//        Snackbar.make(
-//            binding.root,
-//            "Opened",
-//            Snackbar.LENGTH_LONG
-//        ).show()
-//    }
-
-    private fun handleSendText(intent: Intent) {
-        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
-            binding.tvInfo.text = it
+            if(intent.action == Intent.ACTION_VIEW) {
+                Snackbar.make(
+                    binding.root,
+                    "Your geolocation is opened!",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
         }
     }
 
