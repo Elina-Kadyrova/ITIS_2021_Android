056fbf9ce382abf2780865521454ab032dea620e
Elina Kadyrova <kad_elina@mail.ru>
2021-11-17 22:52:35 +0300

    Done Bottom navigation.

--- app/src/main/java/com/itis/firstapp/MainActivity.kt
+++ app/src/main/java/com/itis/firstapp/MainActivity.kt
@@ -3,7 +3,7 @@ package com.itis.firstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
-import androidx.navigation.navOptions
+import androidx.navigation.ui.setupWithNavController
 import com.itis.firstapp.databinding.ActivityMainBinding
 import com.itis.firstapp.extensions.findController
 
@@ -18,66 +18,7 @@ class MainActivity : AppCompatActivity() {
         }
         navController = findController(R.id.host_fragment)
         val bottomNavView = binding?.btmNav
-        //bottomNavView?.setupWithNavController(navController)
-
-       with(binding) {
-            this?.btmNav?.setOnNavigationItemSelectedListener {
-                when (it.itemId) {
-                    R.id.menu_home -> {
-                        onHomeClick()
-                        true
-                    }
-                    R.id.menu_pets -> {
-                        onPetsClick()
-                        true
-                    }
-                    R.id.menu_photos -> {
-                        onPhotosClick()
-                        true
-                    }
-                    else -> false
-                }
-            }
-       }
-    }
-
-    private fun onHomeClick(){
-        val options = navOptions {
-            launchSingleTop = false
-            anim {
-                enter = R.anim.enter_from_right
-                exit = R.anim.exit_to_left
-                popEnter = R.anim.enter_from_left
-                popExit = R.anim.exit_to_right
-            }
-        }
-        navController.navigate(R.id.homeFragment, null, options)
-    }
-
-    private fun onPetsClick(){
-        val options = navOptions {
-            launchSingleTop = false
-            anim {
-                enter = R.anim.enter_from_right
-                exit = R.anim.exit_to_left
-                popEnter = R.anim.enter_from_left
-                popExit = R.anim.exit_to_right
-            }
-        }
-        navController.navigate(R.id.petsFragment, null, options)
-    }
-
-    private fun onPhotosClick(){
-        val options = navOptions {
-            launchSingleTop = false
-            anim {
-                enter = R.anim.enter_from_right
-                exit = R.anim.exit_to_left
-                popEnter = R.anim.enter_from_left
-                popExit = R.anim.exit_to_right
-            }
-        }
-        navController.navigate(R.id.photosFragment, null, options)
+        bottomNavView?.setupWithNavController(navController)
     }
 }
 
