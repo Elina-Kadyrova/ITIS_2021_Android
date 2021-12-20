d464bb142ba4d151b883310d827b80ebbc54f277
Elina Kadyrova <kad_elina@mail.ru>
2021-12-20 17:13:36 +0300

    Change my problem with merging.

--- app/src/main/java/com/itis/firstapp/MainActivity.kt
+++ app/src/main/java/com/itis/firstapp/MainActivity.kt
@@ -2,16 +2,11 @@ package com.itis.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var controller: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        controller = findController(R.id.container)
     }
 }
